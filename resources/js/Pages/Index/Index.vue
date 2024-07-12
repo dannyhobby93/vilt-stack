01afec8f8ceb87606cbbba5d65b9a11153f87d3a
Danny Hobby <danny.hobby@outlook.com>
2024-07-12 15:13:15 +0100

    PROPS

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>Index</div>
+    <div>Message is {{ message }}</div>
+    <Link href="/hello">Show Page</Link>
+</template>
+<script setup>
+import { Link } from "@inertiajs/vue3";
+
+const props = defineProps({
+    message: String,
+});
+</script>
